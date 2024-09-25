9e0e0f836585afa3d04e95c7c5193f9d063e450d
Evelien-Vissers <vissersevelien@gmail.com>
2024-09-25 11:00:07 +0200

    Opdracht 1 voltooid - helperfunctie toegevoegd, syling af en App.jsx af

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import worldMap from './assets/world_map.png'
+import { getColorByRegion} from "./helpers/getColorByRegion.jsx";
 import {useState} from "react";
 
 //'countries' is als state variabele opgehaald om de opgehaalde landeninformatie op te slaan
@@ -10,9 +11,14 @@ function App() {
 
     const fetchCountries = async () => {
         try {
-            const response = await fetch('https://restcountries.com/v3.1/all?fields=name,population,flags');
+            //Gebruik de juiste fields om regio-informatie ook op te halen
+            const response = await fetch('https://restcountries.com/v3.1/all?fields=name,population,flags,region');
             const data = await response.json();
-            setCountries(data);
+
+            // Sorteren van landen op populatie (van laag naar hoog)
+            const sortedCountries = data.sort((a,b) => a.population - b.population);
+
+            setCountries(sortedCountries);
             setIsFetched(true);
         } catch (error) {
             console.error('Error fetching countries:', error);
@@ -23,14 +29,16 @@ function App() {
         <div className="App">
             <h1>Country Information</h1>
             <img src={worldMap} alt="WorldMap" style={{ width: '100%', maxHeight: '500px', objectFit: 'cover' }} />
-            {isFetched && (
+            {!isFetched && (
                 <button onClick={fetchCountries}>Fetch Countries</button>)}
-            <div>
+
+            <div className="countries-container">
                 {countries.map((country) => (
-                    <div key={country.name.commong}>
-                        <img src={country.flags.svg} alt={`Flag of ${country.name.common}`} width="50" />
-                        <p>{country.name.common}</p>
-                        <p>Population: {country.population.toLocaleString()}</p>
+                    /*Voor het identificeren van landen in deze .map functie wordt de unieke cca3 code die elk land heeft gebruikt. Dit zorgt ervoor dat React efficienter met de lijst omgaat. */
+                    <div key={country.cca3}>
+                        <img src={country.flags.svg} alt={`Flag of ${country.name.common}`} width="100" />
+                        <p style={{ color: getColorByRegion(country.region) }}>{country.name.common}</p>
+                        <p>Has a population of {country.population.toLocaleString()} people</p>
                     </div>
                 ))}
             </div>
